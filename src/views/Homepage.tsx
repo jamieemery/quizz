78801e9ab9995be94ace1906a7ea471e160404bf
DESKTOP-20FHI4N\djami <d.jamie.emery@gmail.com>
2023-01-11 18:41:10 +0000

    Inital commit for typescript stuff

---
+++ src/views/Homepage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Container } from "@mui/material";
+
+export const Homepage = () => {
+    return(
+        <Container>
+            Primary</Container>
+    );
+}
+
+export default Homepage;
